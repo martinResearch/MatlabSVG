b76e9f03f2e19a33b9079315356f05222c5ad4b4
martin_delagorce <martin_delagorce@localhost>
2016-08-11 01:05:37 +0100

    adding function to save SVG file

--- loadSVG.m
+++ loadSVG.m
@@ -1,33 +1,32 @@
- /* License FreeBSD:
-    
-     Copyright (c) 2016  Martin de La Gorce
-     All rights reserved.
-     
-     Redistribution and use in source and binary forms, with or without
-     modification, are permitted provided that the following conditions are met:
-     
-     1. Redistributions of source code must retain the above copyright notice, this
-        list of conditions and the following disclaimer.
-     2. Redistributions in binary form must reproduce the above copyright notice,
-        this list of conditions and the following disclaimer in the documentation
-        and/or other materials provided with the distribution.
-     
-     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-     ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-     DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-     ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-     ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-     
-     The views and conclusions contained in the software and documentation are those
-     of the authors and should not be interpreted as representing official policies,
-     either expressed or implied, of the FreeBSD Project.
-
-*/
+%% License FreeBSD:
+%     
+%      Copyright (c) 2016  Martin de La Gorce
+%      All rights reserved.
+%      
+%      Redistribution and use in source and binary forms, with or without
+%      modification, are permitted provided that the following conditions are met:
+%      
+%      1. Redistributions of source code must retain the above copyright notice, this
+%         list of conditions and the following disclaimer.
+%      2. Redistributions in binary form must reproduce the above copyright notice,
+%         this list of conditions and the following disclaimer in the documentation
+%         and/or other materials provided with the distribution.
+%      
+%      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+%      ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+%      WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+%      DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+%      ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+%      (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+%      LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+%      ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%      (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+%      SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%      
+%      The views and conclusions contained in the software and documentation are those
+%      of the authors and should not be interpreted as representing official policies,
+%      either expressed or implied, of the FreeBSD Project.
+%
 
 function [svg]=loadSVG(file)
 if nargin<=1
@@ -59,7 +58,9 @@ for idlayer=0:layers.getLength-1
     layerXml=layers.item(idlayer);
     if layerXml.hasAttribute('inkscape:label')
         layername=char(layerXml.getAttribute('inkscape:label'));
-        
+    else
+        layername=[]
+    end
         allPaths =layerXml.getElementsByTagName('path');
        % fprintf('found %d polygones...',allPaths.getLength)
         createModified=false;
@@ -95,7 +96,7 @@ for idlayer=0:layers.getLength-1
         svg.layers{idlayer+1}=layer;
         
     end
-end
+
 end
 
 
